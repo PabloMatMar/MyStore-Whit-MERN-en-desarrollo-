b157fd8b18e675460d855a05fbd893a9931f876d
PabMatMar <pabmatmar@gmail.com>
2023-05-23 22:02:17 +0200

    add/ testing of the selected game component

--- client/src/components/Main/Products/SelectedGame/SelectedGame.test.js
+++ client/src/components/Main/Products/SelectedGame/SelectedGame.test.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from '@testing-library/react';
 import SelectedGame from "./SelectedGame";
 
 describe("SelectedGame", () => {
   test("matches snapshot", () => {
-    const wrapper = shallow(<SelectedGame />);
-    expect(wrapper).toMatchSnapshot();
+    const selected = {
+      "image": "../assets/mortalKombatUltimateX.jpg",
+      "name": "MORTAL KOMBAT ULTIMATE",
+      "opinion": 4.7,
+      "price": 29.09,
+      "age": 18,
+      "console": "Xbox Series",
+      "exclusiveness": false,
+      "sharedWith": ["Play Station 5", "Nintendo Switch"],
+      "developers": ["NetherRealm Studios", "Shiver Entertainment", "QLOC"],
+      "genre": ["Fight", "Adventure"],
+      "trailer": "https://www.youtube.com/embed/vhjoNZIogRA"
+    }
+    render(<SelectedGame selected={selected} />);
+    expect(screen).toMatchSnapshot();
   });
 });
