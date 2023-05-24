64c3d891359020b3f530fa34f9acbb51ea318428
PabMatMar <pabmatmar@gmail.com>
2023-05-25 00:11:52 +0200

    add/ testing for component SameDevelopers

--- client/src/components/Main/Products/SameDevelopers/SameDevelopers.test.js
+++ client/src/components/Main/Products/SameDevelopers/SameDevelopers.test.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from '@testing-library/react';
 import SameDevelopers from "./SameDevelopers";
 
 describe("SameDevelopers", () => {
   test("matches snapshot", () => {
-    const wrapper = shallow(<SameDevelopers />);
-    expect(wrapper).toMatchSnapshot();
+
+    let developers = "Insomniac Games";
+    const setDevelopers = (setDevelopers) => {
+      return developers = setDevelopers;
+    };
+
+    let products = {
+      "image": "../assets/spiderMan.jpg",
+      "name": "Spider-Man Miles Morales",
+      "opinion": 4.7,
+      "price": 60.25,
+      "age": 16,
+      "console": "Play Station 5",
+      "exclusiveness": true,
+      "sharedWith": [],
+      "developers": ["Insomniac Games"],
+      "genre": ["Adventure"],
+      "trailer": "https://www.youtube.com/embed/Q3kfF3XNzw8",
+    };
+    const setProducts = (setProducts) => {
+      return products = setProducts;
+    };
+
+    render(<SameDevelopers />);
+    expect(screen).toMatchSnapshot();
   });
 });
